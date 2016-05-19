dfb351700a6fcd3a71ca5a0bf593a4b25ca53b3c
Berkutta <info@benjaminmarty.ch>
2016-05-19 11:53:39 +0200

    Placed classes to own file

---
+++ Anpassung/Anpassung.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace helper
+{
+    class Anpassung
+    {
+        double uq, ri, rl, deltaR;
+        int value, numbers;
+
+        public void setUq(double uq)
+        {
+            this.uq = uq;
+        }
+
+        public void setRi(double ri)
+        {
+            this.ri = ri;
+        }
+
+        public void setRl(double rl)
+        {
+            this.rl = rl;
+        }
+
+        public void setDeltaR(double deltaR)
+        {
+            this.deltaR = deltaR;
+        }
+
+        public void setValue(int value)
+        {
+            this.value = value;
+        }
+
+        public void setNumbers(int numbers)
+        {
+            this.numbers = numbers;
+        }
+
+        public int getValue()
+        {
+            return this.value;
+        }
+
+        public int getNumbers()
+        {
+            return this.numbers;
+        }
+
+        public double getRl()
+        {
+            return this.getValue() * this.deltaR;
+        }
+
+        public double getIl()
+        {
+            return this.uq / (this.ri + this.getRl());
+        }
+
+        public double getUk()
+        {
+            return this.uq - (this.ri * this.getIl());
+        }
+
+        public double getPl()
+        {
+            return this.getUk() * this.getIl();
+        }
+    }
+    
+}
---
+++ Anpassung/Engineering.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace helper
+{
+    class Engineering
+    {
+        double value;
+
+        public void setValue(double value)
+        {
+            this.value = value;
+        }
+
+        public void setEngineering(string engineering)
+        {
+            this.value = Convert.ToDouble(engineering.TrimEnd('f', 'p', 'n', 'u', 'µ', 'm', 'k', 'M', 'G'));
+
+            if (engineering.Contains('f'))
+            {
+                this.value = this.value * Math.Pow(10, -15);
+            }
+            else if (engineering.Contains('p'))
+            {
+                this.value = this.value * Math.Pow(10, -12);
+            }
+            else if (engineering.Contains('n'))
+            {
+                this.value = this.value * Math.Pow(10, -9);
+            }
+            else if (engineering.Contains('u'))
+            {
+                this.value = this.value * Math.Pow(10, -6);
+            }
+            else if (engineering.Contains('µ'))
+            {
+                this.value = this.value * Math.Pow(10, -6);
+            }
+            else if (engineering.Contains('m'))
+            {
+                this.value = this.value * Math.Pow(10, -3);
+            }
+            else if (engineering.Contains('k'))
+            {
+                this.value = this.value * Math.Pow(10, 3);
+            }
+            else if (engineering.Contains('M'))
+            {
+                this.value = this.value * Math.Pow(10, 6);
+            }
+            else if (engineering.Contains('G'))
+            {
+                this.value = this.value * Math.Pow(10, 9);
+            }
+        }
+
+        public double getValue()
+        {
+            return this.value;
+        }
+
+        public string getEngineering()
+        {
+            string buffer;
+            double number_buffer;
+
+            if ((this.value >= Math.Pow(10, -15)) && (this.value < Math.Pow(10, -12)))
+            {
+                number_buffer = this.value / Math.Pow(10, -15);
+                buffer = number_buffer.ToString("0.00");
+
+                return buffer + "f";
+            }
+            else if ((this.value >= Math.Pow(10, -12)) && (this.value < Math.Pow(10, -9)))
+            {
+                number_buffer = this.value / Math.Pow(10, -12);
+                buffer = number_buffer.ToString("0.00");
+
+                return buffer + "p";
+            }
+            else if ((this.value >= Math.Pow(10, -9)) && (this.value < Math.Pow(10, -6)))
+            {
+                number_buffer = this.value / Math.Pow(10, -9);
+                buffer = number_buffer.ToString("0.00");
+
+                return buffer + "n";
+            }
+            else if ((this.value >= Math.Pow(10, -6)) && (this.value < Math.Pow(10, -3)))
+            {
+                number_buffer = this.value / Math.Pow(10, -6);
+                buffer = number_buffer.ToString("0.00");
+
+                return buffer + "u";
+            }
+            else if ((this.value >= Math.Pow(10, -3)) && (this.value < Math.Pow(10, 0)))
+            {
+                number_buffer = this.value / Math.Pow(10, -3);
+                buffer = number_buffer.ToString("0.00");
+
+                return buffer + "m";
+            }
+            else if ((this.value >= Math.Pow(10, 0)) && (this.value < Math.Pow(10, 3)))
+            {
+                number_buffer = this.value;
+                buffer = number_buffer.ToString("0.00");
+
+                return buffer;
+            }
+            else if ((this.value >= Math.Pow(10, 3)) && (this.value < Math.Pow(10, 6)))
+            {
+                number_buffer = this.value / Math.Pow(10, 3);
+                buffer = number_buffer.ToString("0.00");
+
+                return buffer + "k";
+            }
+            else if ((this.value >= Math.Pow(10, 6)) && (this.value < Math.Pow(10, 9)))
+            {
+                number_buffer = this.value / Math.Pow(10, 6);
+                buffer = number_buffer.ToString("0.00");
+
+                return buffer + "M";
+            }
+            else if ((this.value >= Math.Pow(10, 9)) && (this.value < Math.Pow(10, 12)))
+            {
+                number_buffer = this.value / Math.Pow(10, 9);
+                buffer = number_buffer.ToString("0.00");
+
+                return buffer + "G";
+            }
+
+            // In case of error return raw string value
+            return this.value.ToString("0.00");
+        }
+    }
+}
